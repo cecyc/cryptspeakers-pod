86681639a51b2a68fab2709e3dc5ea20a900b175
Cecy Correa <cecycorrea@gmail.com>
2021-01-07 16:52:53 -0600

    fix navbar spacing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
   <nav class="container">
-    <h1><a href="/" class="home-btn">CryptSpeakers</a></h1>
-    <ul class="nav-links">
-      <li><a href="/about" class="nav-link">About</a></li>
-      <li><a href="/episodes" class="nav-link">Episodes</a></li>
-    </ul>
+    <div class="row">
+      <div class="col-sm-6 nav-title">
+        <h1><a href="/" class="home-btn">CryptSpeakers</a></h1>
+      </div>
+      <div class="col-sm-6">
+        <ul class="nav-links">
+          <li><a href="/about" class="nav-link">About</a></li>
+          <li><a href="/episodes" class="nav-link">Episodes</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 <style lang="scss" scoped>
@@ -24,20 +30,32 @@ nav ul {
   padding: 0 !important;
 }
 
+.nav-title {
+  text-align: center;
+}
+
 .nav-links {
-  margin: 0 0 0 2rem;
-  margin-left: 2rem;
   display: flex;
+  justify-content: center;
+
+  @media screen and (min-width: 600px) {
+    margin: 0 0 0 2rem;
+    margin-left: 2rem;
+  }
 }
 
 .nav-link {
+  font-size: 14px;
+  font-weight: bold;
   border-bottom: 2px solid transparent;
   color: var(--indigo);
   font-family: 'Montserrat', Arial, Helvetica, sans-serif;
-  font-size: 18px;
-  font-weight: bold;
-  margin-left: 2rem;
   margin-top: 2px;
+
+  @media screen and (min-width: 600px) {
+    font-size: 18px;
+    margin-left: 2rem;
+  }
 }
 
 .nav-link:hover {
